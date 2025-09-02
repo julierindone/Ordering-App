2c01066e48192c2e95b26ac25e407d344a723d77
Julie Rindone <julie.rindone@gmail.com>
2025-09-01 17:08:28 -0700

    Created html & handling to add/display selected items and total

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 import { menuArray } from "./data.js";
 
 const menuEl = document.getElementById('menu')
+const orderSummaryEl = document.getElementById('order-summary')
+const selectionListEl = document.getElementById('selection-list')
+const orderTotalEl = document.getElementById('order-total')
+let orderTotal = 0
+
+document.addEventListener('DOMContentLoaded', renderMenu())
+
+document.addEventListener('click', (event) => {
+	if (event.target.dataset.item) {
+		handleOrderSummary(event.target.dataset.item)
+	}
+})
+
+function handleOrderSummary(itemId) {
+	// find item associated with that ID.
+	const selectedItemObject =
+		menuArray.filter((menuItem) => {
+			return menuItem.id === parseInt(itemId)
+		})[0]
+
+	// Add item to selection list
+	selectionListEl.innerHTML += `
+		<div class="selection">
+			<div class="item-name-and-remove-btn">
+			<p class="item-name">${selectedItemObject.name}</p>
+			</div>
+			<p>$${selectedItemObject.price}</p>
+		</div>
+	`
+
+	// TODO: update + to - on the button next to the item in the menu. This will need to be able to be reversed, also.
+	// Add price to orderTotal (// TODO: will also need to add handling to subtract)
+	orderTotal += selectedItemObject.price
+	orderTotalEl.innerHTML = `$${orderTotal}`
+
+	// update order-summary display to flex if not already displayed
+	orderSummaryEl.style.display = 'none' && (orderSummaryEl.style.display = 'flex')
+}
 
 function createMenuHtml() {
 	const mapMenu = menuArray.map(item => {
@@ -14,7 +52,7 @@ function createMenuHtml() {
 				</p>
 			</div>
 			<div class="add-remove">
-				<button id="add-remove-btn"><span>+</span></button>
+				<button class="add-remove-btn" type="click" data-item="${item.id}">+</button>
 			</div>
 		</div>`
 	})
@@ -24,5 +62,3 @@ function createMenuHtml() {
 function renderMenu() {
 	menuEl.innerHTML = createMenuHtml()
 }
-
-renderMenu()
